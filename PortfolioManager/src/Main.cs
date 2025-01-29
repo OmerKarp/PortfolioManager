bab6fb55bf0a01275611062d59c65ca268c1d25b
OmerKarp <omerkarp1306@gmail.com>
2025-01-30 00:08:54 +0200

    Restructured the files. also aded Main function.

---
+++ PortfolioManager/src/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PortfolioManager.src
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Portfolio Manager!");
+            Console.ReadKey();
+        }
+    }
+}
